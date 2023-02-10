c1921f3d8c48a881476d9d25831731baefebfcc7
CapelHoworth <capelhoworth@gmail.com>
2023-02-10 13:12:26 -0500

    freeshelf requirements met

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import "./App.css";
+import placeholder from "./ImageError.jpeg";
+
+const fixBrokenImage = (e) => {
+  e.target.src = placeholder;
+};
 
 function App({ books }) {
   return (
     <div className="App">
-      <Books books={books} />
+      {books.map((book) => (
+        <Books book={book} />
+      ))}
+      ;
     </div>
   );
 }
 
-function Books({ books }) {
+function Books({ book }) {
+  const [showResults, setShowResults] = useState(false);
+  const onExpand = () => setShowResults(!showResults);
   return (
     <div>
       <h1>Books!</h1>
       <div className="App">
-        {books.map((book) => (
-          <div className="book-list">
+        <div className="book-list">
+          <div>
             <h2>{book.title}</h2>
             <h3>{book.author}</h3>
-            <img src={book.coverImageUrl} className="cover-art" alt="cover" />
-            <p>{book.shortDescription}</p>
+            <img
+              src={book.coverImageUrl}
+              className="cover-art"
+              alt="cover"
+              onError={fixBrokenImage}
+            />
+            <br />
+            <button onClick={onExpand}>Show Book Bio</button>
+            {showResults === true && <div>{book.shortDescription}</div>}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
